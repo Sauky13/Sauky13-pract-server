41ac7a0b07414588b411e50dcb9bc042cc78ac17
Рина <kremis8@mail.ru>
2024-03-27 21:32:30 +0300

    Задание 2
    Авторизация Регистрация
    Не авторизованный пользователь пересылается на страницу авторизации

--- views/site/signup.php
+++ views/site/signup.php
@@ -1,6 +1,8 @@
-<h2>Регистрация нового пользователя</h2>
+<h2 style="     display: flex;
+    justify-content: center;">Регистрация нового пользователя</h2>
 <h3><?= $message ?? ''; ?></h3>
-<form method="post">
+<form method="post" style=" display: flex; gap: 10px; flex-direction: column;
+    align-items: center;">
    <label>Имя <input type="text" name="name"></label>
    <label>Логин <input type="text" name="login"></label>
    <label>Пароль <input type="password" name="password"></label>
