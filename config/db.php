a22872b75d5bd92a1b5232d2a3c26349964f3940
Sauky13 <kremis8@mail.ru>
2024-03-27 23:03:36 -0700

    БД

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 return [
     'driver' => 'mysql',
     'host' => 'localhost',
-    'database' => 'mvc',
-    'username' => 'root',
-    'password' => '',
+    'database' => 'imzgmegt_m6',
+    'username' => 'imzgmegt',
+    'password' => 'c6SLBX',
     'charset' => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix' => '',
