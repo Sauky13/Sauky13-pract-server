1ccec684e675376c790e1de3fc5621d447f7ee29
Sauky13 <kremis8@mail.ru>
2024-03-28 01:13:15 -0700

    Верстка страниц для администратора и сотрудника

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -6,24 +6,33 @@
          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
    <meta http-equiv="X-UA-Compatible" content="ie=edge">
    <title>Pop it MVC</title>
+
 </head>
-<body>
+<body style="margin:10px;   font-size:14px;
+    font-family:Verdana;">
 <header>
-   <nav>
-       <a href="<?= app()->route->getUrl('/hello') ?>">Главная</a>
-       <?php
-       if (!app()->auth::check()):
-           ?>
-           <a href="<?= app()->route->getUrl('/login') ?>">Вход</a>
-           <a href="<?= app()->route->getUrl('/signup') ?>">Регистрация</a>
-       <?php
-       else:
-           ?>
-           <a href="<?= app()->route->getUrl('/logout') ?>">Выход (<?= app()->auth::user()->name ?>)</a>
-       <?php
-       endif;
-       ?>
-   </nav>
+    <nav>
+        <?php
+        if (!app()->auth::check()):
+            ?>
+            <a style="text-decoration: none; color: #3d3d3d; font-weight:600" href="<?= app()->route->getUrl('/login') ?>">Авторизация</a>
+        <?php
+        else:
+            ?>
+            <a style="text-decoration: none; color: #3d3d3d; font-weight:600" href="<?= app()->route->getUrl('/logout') ?>">Выйти</a>
+            <?php
+            if (app()->auth::user()->role_id === 1):
+                ?>
+                <p style="font-weight: 600; color: #9E9E9E; font-size: xx-small;">Вы вошли как админ</p>
+            <?php
+            elseif (app()->auth::user()->role_id === 2):
+                ?>
+                <p style="font-weight: 600; color: #9E9E9E; font-size: xx-small;">Вы вошли как сотрудник</p>
+            <?php
+            endif;
+        endif;
+        ?>
+    </nav>
 </header>
 <main>
    <?= $content ?? '' ?>
--- views/site/login.php
+++ views/site/login.php
@@ -5,9 +5,10 @@
 <?php
 if (!app()->auth::check()):
    ?>
-   <form method="post">
-       <label>Логин <input type="text" name="login"></label>
-       <label>Пароль <input type="password" name="password"></label>
-       <button>Войти</button>
+   <form  style="display: flex; gap: 20px; flex-direction: column; width: 800px"  method="post">
+       <label> <input type="text" name="login" placeholder="Логин" style="background: rgba(236, 236, 236, 1);border: none;border-radius: 5px;height: 22px;font-weight: 600;"></label>
+       <label> <input type="password" name="password" placeholder="Пароль" style="background: rgba(236, 236, 236, 1);border: none;border-radius: 5px;height: 22px;font-weight: 600;"></label>
+       <button style="width: 170px; border: none;border-radius: 5px; background-color: rgba(170, 170, 170, 1); color: white; font-weight: 600;">Войти</button>
    </form>
 <?php endif;
+
