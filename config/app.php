42ef1d89c8103c6cc21e38f88e182ef5206c0f71
Рина <kremis8@mail.ru>
2024-04-03 22:56:35 +0300

    4 задание готовое

--- config/app.php
+++ config/app.php
@@ -10,17 +10,12 @@ return [
         'auth' => \Middlewares\AuthMiddleware::class,
     ],
     'routeAppMiddleware' => [
-<<<<<<< HEAD
+        
         'csrf' => middlewares\CSRFMiddleware::class,
         'trim' => middlewares\TrimMiddleware::class,
         'specialChars' => middlewares\SpecialCharsMiddleware::class,
     ],
-=======
-        'csrf' => \Middlewares\CSRFMiddleware::class,
-        'trim' => \Middlewares\TrimMiddleware::class,
-        'specialChars' => \Middlewares\SpecialCharsMiddleware::class,
-     ],
->>>>>>> a8157853a8246c08b1e320f46a2d26c7039cc3cd
+
      
      
     'validators' => [
